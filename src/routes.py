33a7796dd1fff8b35f90bf6c200b6041e5a6e9d5
bit-web24 <bitweb24@gmail.com>
2023-05-21 00:29:37 +0530

    defined proper routes and functionality

--- src/routes.py
+++ src/routes.py
@@ -1,39 +1,113 @@
 from flask import Flask
-from flask import jsonify
-from schema import connect
+from schema import schema_init
+import sqlite3
+import json
+import os
 
 app = Flask(__name__)
+
+schema_init()
+
+# Get the project directory absolute path
+root_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = root_dir + '/../db/database.db'
 
 # Implement the /users endpoint to return a list of all users:
 @app.route('/users', methods=['GET'])
 def get_users():
-    # Retrieve all users from the database
-    users = connect.execute("")
-    # Convert the data to a JSON format
-    # Return the JSON response
-    return "list of all users"
+    connect = sqlite3.connect(db_path)
+    cursor = connect.cursor()
+    cursor.execute('''SELECT * FROM users''')
+    users = cursor.fetchall()
+    
+    users_list = []
+
+    for user in users:
+        user_dict = {
+                'id': user[0],
+                'name': user[1],
+                'email': user[2],
+                'created_at': user[3]
+                }
+        users_list.append(user_dict)
+
+    users_json = json.dumps(users_list)
+    return users_json
 
 # Implement the /users/{id} endpoint to return the details of a specific user:
 @app.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    # Retrieve the user with the given ID from the database
-    # Convert the user data to a JSON format
-    # Return the JSON response
-    return "details of a user"
+    connect = sqlite3.connect(db_path)
+    cursor = connect.cursor()
+    cursor.execute('''SELECT * FROM users WHERE id = ?''', (user_id,))
+    user = cursor.fetchone()
+
+    if user:
+        user_dict = {
+                'id': user[0],
+                'name': user[1],
+                'email': user[2],
+                'created_at': user[3]
+                }
+        
+        user_json = json.dumps(user_dict)
+        return user_json
+    else:
+        msg = {
+                "error": "User not found"
+                }
+        err = json.dumps(msg)
+        return err
 
 # Implement the /orders endpoint to return a list of all orders:
 @app.route('/orders', methods=['GET'])
 def get_orders():
-    # Retrieve all orders from the database
-    # Convert the data to a JSON format
-    # Return the JSON response
-    return "list of all oreders"
+    connect = sqlite3.connect(db_path)
+    cursor = connect.cursor()
+    cursor.execute('''SELECT * FROM orders''')
+    orders = cursor.fetchall()
+
+    orders_list = []
+
+    for order in orders:
+        order_dict = {
+                'id': order[0],
+                'user_id': order[1],
+                'product_name': order[2],
+                'quantity': order[3],
+                'total_price': order[4],
+                'created_at': order[5]
+                }
+        
+        orders_list.append(order_dict)
+    
+    orders_json = json.dumps(orders_list)
+    return orders_json
 
 # Implement the /orders/{id} endpoint to return the details of a specific order:
 @app.route('/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
-    # Retrieve the order with the given ID from the database
-    # Convert the order data to a JSON format
-    # Return the JSON response
-    return "details of a specific order"
+   connect = sqlite3.connect(db_path)
+   cursor = connect.cursor()
+   cursor.execute('''SELECT * FROM orders WHERE id = ?''', (order_id,))
+   order = cursor.fetchone()
+
+   if order:
+       order_dict = {
+               'id': order[0],
+               'user_id': order[1],
+               'product_name': order[2],
+               'quantity': order[3],
+               'total_price': order[4],
+               'created_at': order[5]
+               }
+
+       order_json = json.dumps(order_dict)
+       return order_json
+   else:
+       msg = {
+               "error": "Order not found"
+               }
+       err = json.dumps(msg)
+       return err
 
--- src/schema.py
+++ src/schema.py
@@ -1,45 +1,48 @@
 import sqlite3
 from seed import users_data, orders_data
+import os
 
-connect = sqlite3.connect('./db/database.db')
+def schema_init():
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    connect = sqlite3.connect(root_dir + '/../db/database.db')
+    print('connection to database [OK]')
 
-print('connection to database [OK]')
+    # Create a cursor object to execute SQL statements
+    cursor = connect.cursor()
 
-# Create a cursor object to execute SQL statements
-cursor = connect.cursor()
-
-# Drop the 'users' and 'orders' tables if they exist
-cursor.execute('''DROP TABLE IF EXISTS users''')
-cursor.execute('''DROP TABLE IF EXISTS orders''')
-
-# Create the 'users' table
-connect.execute('''CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    email TEXT,
-    created_at TIMESTAMP
-)''')
-
-print("creating users table [OK]")
-
-# Create the 'orders' table
-connect.execute('''CREATE TABLE orders (
-    id INTEGER PRIMARY KEY,
-    user_id INTEGER,
-    product_name TEXT,
-    quantity INTEGER,
-    total_price REAL,
-    created_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-)''')
-
-print("creating orders table [OK]")
-
-# Execute insert command for all users_data
-cursor.executemany('''INSERT INTO users (id, name, email, created_at) VALUES (?, ?, ?, ?)''', users_data)
-
-# Execute insert command for all orders_data
-cursor.executemany('''INSERT INTO orders (id, user_id, product_name, quantity, total_price, created_at) VALUES (?, ?, ?, ?, ?, ?)''', orders_data)
-
-# Commit the changes and close the connection
-connect.commit()
+    # Drop the 'users' and 'orders' tables if they exist
+    cursor.execute('''DROP TABLE IF EXISTS users''')
+    cursor.execute('''DROP TABLE IF EXISTS orders''')
+    
+    # Create the 'users' table
+    connect.execute('''CREATE TABLE users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        created_at TIMESTAMP
+    )''')
+    
+    print("creating users table [OK]")
+    
+    # Create the 'orders' table
+    connect.execute('''CREATE TABLE orders (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        product_name TEXT,
+        quantity INTEGER,
+        total_price REAL,
+        created_at TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )''')
+    
+    print("creating orders table [OK]")
+    
+    # Execute insert command for all users_data
+    cursor.executemany('''INSERT INTO users (id, name, email, created_at) VALUES (?, ?, ?, ?)''', users_data)
+    
+    # Execute insert command for all orders_data
+    cursor.executemany('''INSERT INTO orders (id, user_id, product_name, quantity, total_price, created_at) VALUES (?, ?, ?, ?, ?, ?)''', orders_data)
+    
+    # Commit the changes and close the connection
+    connect.commit()
+    connect.close()
